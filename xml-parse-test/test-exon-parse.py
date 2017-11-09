8cdc6ed8e3e4bb46265629c3437eabd5eb3663d2
Theo Cole <theo.j.cole@gmail.com>
2017-11-09 13:56:21 +0000

    finished the test implementation of xml parser

--- xml-parse-test/test-exon-parse.py
+++ xml-parse-test/test-exon-parse.py
@@ -68,7 +68,7 @@ def parse_exon_positions(lrg_file):
 
     }
 
-    """  
+    """
 
     position_dict = {}
 
@@ -79,15 +79,12 @@ def parse_exon_positions(lrg_file):
 
     lrg_id = fixed.find("id").text
     position_dict["lrg_id"] = lrg_id
-    print lrg_id
 
     transcripts = lrg_root.iter("transcript")
     for transcript in transcripts:
         if 'name' in transcript.attrib:
             transcript_name = transcript.attrib['name']
-            print transcript_name
             transcript_dict = position_dict[transcript_name] = {}
-
 
             transcript_exons = [
                 x for x in transcript.findall("exon") if "label" in x.attrib
@@ -101,6 +98,23 @@ def parse_exon_positions(lrg_file):
                         exon_position_dict["start"] = coords.attrib["start"]
                         exon_position_dict["end"] = coords.attrib["end"]
 
+    mapping_annotation = None
+
+    annotation_sets = updatable.findall("annotation_set")
+    for annotation_set in annotation_sets:
+        if annotation_set.attrib["type"] == "lrg":
+            mapping_annotation = annotation_set
+
+    assert mapping_annotation is not None
+
+    for mapping_coords in mapping_annotation.findall("mapping"):
+        build_name = "build_" + \
+            mapping_coords.attrib["coord_system"].split('.')[0]
+        build_dict = position_dict[build_name] = {}
+        build_dict["start"] = mapping_coords.attrib["other_start"]
+        build_dict["end"] = mapping_coords.attrib["other_end"]
+
     return position_dict
+
 
 pprint.pprint(parse_exon_positions(sys.argv[1]))
