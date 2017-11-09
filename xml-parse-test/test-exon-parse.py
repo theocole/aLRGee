e500b24b43a50ccee5cf3c6fae658c12cec55bc3
Theo Cole <theo.j.cole@gmail.com>
2017-11-09 15:25:37 +0000

    edited comments to make them more readable

--- xml-parse-test/test-exon-parse.py
+++ xml-parse-test/test-exon-parse.py
@@ -10,64 +10,6 @@ def parse_exon_positions(lrg_file):
     coordinate system, returning a dictionary of relative exon positions
     with the following structure (and start/stop positions on each genome
     build.
-
-    Usage
-    -----
-    xml_parser("LRG_9.xml")
-     -->
-        sample_dict = {
-        'lrg_id': "LRG_X",
-        'transcript1': {
-            'relative_exon_positions': {
-                'exon1': {
-                    'start': 128,
-                    'stop': 345
-                }
-                'exon2': {
-                    'start': 745,
-                    'stop': 923
-                }
-                'exon3': {
-                    'start': 1184,
-                    'stop': 1592
-                }
-                'exon4': {
-                    'start': 1808,
-                    'stop': 2140
-                }
-                'exon5': {
-                    'start': 2781,
-                    'stop': 3019
-                }
-            }
-        }
-        'transcript2': {
-            'relative_exon_positions': {
-                'exon1': {
-                    'start': 80,
-                    'stop': 194
-                }
-                'exon2': {
-                    'start': 555,
-                    'stop': 801
-                }
-                'exon3': {
-                    'start': 958,
-                    'stop': 1104
-                }
-            }
-        }
-        'build_GRCh37': {
-            'mapping_start': 187429875,
-            'mapping_stop': 187431770,
-        }
-        'build_GRCh38': {
-            'mapping_start': 179354041,
-            'mapping_stop': 179357191,
-        }
-
-    }
-
     """
 
     position_dict = {}
