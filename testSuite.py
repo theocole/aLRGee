3506fedefd34e1540df2fe6fc593bcf8881f75e9
Theo Cole <theo.j.cole@gmail.com>
2017-11-12 19:36:16 +0000

    added test suite

---
+++ testSuite.py
@@ -0,0 +1,23 @@
+import get_exon_shifts
+
+
+class TestLRGScraper(object):
+    """docstring for TestLRGScraper"""
+    def __init__(self, arg):
+        super(TestLRGScraper, self).__init__()
+        self.arg = arg
+
+
+class TestLRGParser(object):
+    """docstring for TestLRGParser"""
+    def __init__(self, arg):
+        super(TestLRGParser, self).__init__()
+        self.arg = arg
+        
+
+class TestResultsCalc(object):
+    """docstring for TestResultsCalc"""
+    def __init__(self, arg):
+        super(TestResultsCalc, self).__init__()
+        self.arg = arg
+        
