be630752cb58d0a59b31ecb7ab4946a805763621
Theo Cole <theo.j.cole@gmail.com>
2017-11-13 09:29:38 +0000

    added test classes for all functions in get_exon_shifts

---
+++ test_suite.py
@@ -0,0 +1,37 @@
+import get_exon_shifts
+
+
+class TestLRGScraper(object):
+    """docstring for TestLRGScraper"""
+    def __init__(self, arg):
+        super(TestLRGScraper, self).__init__()
+        self.arg = arg
+
+
+class TestLRGParser(object):
+    """docstring for TestLRGParser"""
+    def __init__(self, arg):
+        super(TestLRGParser, self).__init__()
+        self.arg = arg
+        
+
+class TestPlotExonShifts(object):
+    """docstring for TestPlotExonShifts"""
+    def __init__(self, arg):
+        super(TestPlotExonShifts, self).__init__()
+        self.arg = arg
+
+
+class TestCalcGenomicPosition(object):
+    """docstring for TestCalcGenomicPosition"""
+    def __init__(self, arg):
+        super(TestCalcGenomicPosition, self).__init__()
+        self.arg = arg
+
+
+class TestDisplayResults(object):
+    """docstring for TestDisplayResults"""
+    def __init__(self, arg):
+        super(TestDisplayResults, self).__init__()
+        self.arg = arg
+        
